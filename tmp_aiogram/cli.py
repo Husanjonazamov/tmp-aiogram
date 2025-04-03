f494bd2efa4a1c6c5b1c36e007e11e083f6209c7
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-03 19:37:23 +0500

    `create_folder_structure` funksiyasini yangilandi va `project_name` argumenti qo'shildi. Terminalda `tmp create` komandasi yordamida loyiha strukturasini yaratish uchun `click` kutubxonasi ishlatildi.

--- tmp_aiogram/cli.py
+++ tmp_aiogram/cli.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import click
 
 
-def create_folder_structure():
+def create_folder_structure(project_name):
     """ Yaratilishi kerak bo'lgan papkalar va fayllar """
     folders = [
         "handlers", "utils", "services", "state"
@@ -21,34 +22,41 @@ def create_folder_structure():
         "state": ["__init__.py", "state.py"]
     }
 
+    if not os.path.exists(project_name):
+        os.makedirs(project_name)
+
     for folder in folders:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+        folder_path = os.path.join(project_name, folder)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
     for file in files:
-        with open(file, "w") as f:
+        file_path = os.path.join(project_name, file)
+        with open(file_path, "w") as f:
             f.write("")
 
     for folder, sub_files in files_structure.items():
         for sub_file in sub_files:
-            file_path = os.path.join(folder, sub_file)
+            folder_path = os.path.join(project_name, folder)
+            file_path = os.path.join(folder_path, sub_file)
             with open(file_path, "w") as f:
                 if sub_file == "__init__.py":
                     f.write("from . import start\n" if folder == "handlers" else "pass\n")
                 else:
                     f.write(f"# {sub_file} fayli\n")
     
-    print("Paket struktura yaratildi!")
+    print(f"✅ {project_name} loyihasi struktura yaratildi!")
 
 
-def create():
-    """ `tmp create` komandasini terminalda bajarish uchun """
-    print("Paket strukturasini yaratish uchun tasdiqlang (y/n): ")
+@click.command()
+@click.argument('project_name')
+def create(project_name):
+    """ `tmp create project_name` komandasini terminalda bajarish uchun """
+    print(f"{project_name} nomli loyihani yaratishni tasdiqlang (y/n): ")
     confirm = input().strip().lower()
     
     if confirm == "y":
-        create_folder_structure()
-        print("✅ Paket struktura yaratildi!")
+        create_folder_structure(project_name)
     else:
         print("❌ Yaratish bekor qilindi.")
         sys.exit(0)
